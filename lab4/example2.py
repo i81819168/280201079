9dbc181811a35ac4568305d6d18443efb9059743
i81819168 <elifozyurek@std.iyte.edu.tr>
2020-11-09 11:33:47 +0000

    ex2

--- lab4/example2.py
+++ lab4/example2.py
@@ -1,11 +1,12 @@
 year=int(input("enter a year:"))
-if year<100:
-  if year%4==0:
+if year%4==0:
+  if year%100==0:
+    if year%400==0:
+      print("leap year")
+    else:
+      print("not leap year")
+  else:
     print("leap year")
-  else:
-    print("not leap year")
-elif year%400==0:
-  print("leap year")
-  else:
-    print("not leap year")
+else:
+  print("not leap year")        
 
